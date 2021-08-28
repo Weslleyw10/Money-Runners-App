759f1e6bffc9b5984336110f4c12ad823b096444
Weslleyw10 <weslleylopes.dev@gmail.com>
2021-08-28 14:30:37 -0300

    Add Url Prod WS

--- src/services/rest.js
+++ src/services/rest.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const rest = axios.create({
-    baseURL: __DEV__ ? "http://192.168.0.104:8000" : ''
+    baseURL: __DEV__ ? "http://192.168.0.104:8000" : 'https://wls-ws-money-runners.herokuapp.com'
 })
 
 export default rest
